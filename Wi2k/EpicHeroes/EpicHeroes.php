9c4564081605d49f8f4ae5a7b477f28b9eca3718
Vasync <tbao12901@gmail.com>
2024-08-26 19:01:47 +0700

    create EpicHeroes.php

---
+++ Wi2k/EpicHeroes/EpicHeroes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wi2k\EpicHeroes;
+
+use pocketmine\plugin\PluginBase;
+
+class EpicHeroes extends PluginBase {
+
+    protected function onEnable(): void {
+    }
+}
